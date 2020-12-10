de56d7111fe94bf65a1cacca3757994293e51318
ahpoi <ianah1095@hotmail.com>
2020-12-10 21:58:36 +1100

    chore: Test husky

--- src/components/molecules/input-fields.tsx
+++ src/components/molecules/input-fields.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import { FieldErrorMessage, FieldLabel, Input, Label, Select, TextArea, TextAreaProps } from "../..";
 
 type Props = {
-
   label: string;
   star?: boolean
 
