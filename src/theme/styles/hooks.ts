683dc4368771083f6a829dd26e453bff0a9f4d3e
ahpoi <ianah1095@hotmail.com>
2021-04-09 19:40:06 +1000

    feat: Improve useColor scheme lookup

--- src/theme/styles/hooks.ts
+++ src/theme/styles/hooks.ts
@@ -9,15 +9,16 @@ import { useTheme } from "styled-components";
  * Eg: useColorScheme(white, primary) -> white
  * */
 export const useColorScheme = (color: string, scheme?: ColorScheme) => {
+  const _colors = useTheme().colors;
   if (color.split(".").length === 2) {
     const [colorKey, colorNumber] = color.split(".");
-    if (useTheme().colors[colorKey as never]) {
-      return useTheme().colors[colorKey as never][colorNumber as string];
+    if (_colors[colorKey as never]) {
+      return _colors[colorKey as never][colorNumber as string];
     }
     return color;
   }
   if (["50", "100", "200", "300", "400", "500", "600", "700", "800", "900"].includes(color) && scheme) {
-    return useTheme().colors?.[scheme as never]?.[color] ?? color;
+    return _colors?.[scheme as never]?.[color] ?? color;
   }
   return color;
 };
